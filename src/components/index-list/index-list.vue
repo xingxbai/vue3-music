7d56c56dbb186c703f75a9535eb761ea01f888eb
xingxbai <baixingxinglive@163.com>
2021-08-17 22:23:03 +0800

    添加setupuseScroll

--- src/components/index-list/index-list.vue
+++ src/components/index-list/index-list.vue
@@ -1,112 +1,131 @@
 <template>
-    <scroll
-        class="index-list"
-        :probe-type="3"
-        @scroll="onScroll"
-        ref="scrollRef"
-    >
-        <ul ref="groupRef">
-            <li v-for="group in data" :key="group.title" class="group">
-                <div class="title">{{ group.title }}</div>
-                <ul>
-                    <li
-                        v-for="singer in group.list"
-                        :key="singer.id"
-                        class="item"
-                    >
-                        <img class="avatar" v-lazy="singer.pic" />
-                        <span class="name">{{ singer.name }}</span>
-                    </li>
-                </ul>
-            </li>
+  <scroll class="index-list" :probe-type="3" @scroll="onScroll" ref="scrollRef">
+    <ul ref="groupRef">
+      <li v-for="group in data" :key="group.title" class="group">
+        <div class="title">{{ group.title }}</div>
+        <ul>
+          <li v-for="singer in group.list" :key="singer.id" class="item">
+            <img class="avatar" v-lazy="singer.pic" />
+            <span class="name">{{ singer.name }}</span>
+          </li>
         </ul>
-    </scroll>
+      </li>
+    </ul>
+    <ul class="shortcut" @touchstart.stop.prevent="onShortcutTouchStart">
+      <li
+        class="item"
+        v-for="(group, index) in data"
+        :key="group.title"
+        :data-index="index"
+        :class="{ current: currentIndex === index }"
+      >
+        {{ group.title }}
+      </li>
+    </ul>
+    <div class="fixed" :style="fixedStyle">
+      <div class="fixed-title">{{ fixedTitle }}</div>
+    </div>
+  </scroll>
 </template>
 
 <script>
-import scroll from '../scroll/scroll';
+import scroll from "../scroll/scroll";
+import useFixed from "./use-fixed";
 export default {
-    name: 'index-list',
-    components: {
-        scroll
-    },
-    props: {
-        data: Array
-    },
-    methods: {
-        onScroll() {}
-    }
+  name: "index-list",
+  components: {
+    scroll
+  },
+  props: {
+    data: Array
+  },
+  setup(props) {
+    const {
+      onScroll,
+      groupRef,
+      fixedTitle,
+      currentIndex,
+      fixedStyle
+    } = useFixed(props);
+    return {
+      onScroll,
+      groupRef,
+      fixedTitle,
+      currentIndex,
+      fixedStyle
+    };
+  }
 };
 </script>
 
-
 <style lang="scss" scoped>
 .index-list {
-    position: relative;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: $color-background;
+  .group {
+    padding-bottom: 30px;
+    .title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background: $color-highlight-background;
+      text-align: left;
+    }
+    .item {
+      display: flex;
+      align-items: center;
+      padding: 20px 0 0 30px;
+      .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+      }
+      .name {
+        margin-left: 20px;
+        color: $color-text-l;
+        font-size: $font-size-medium;
+      }
+    }
+  }
+  .fixed {
+    position: absolute;
+    top: -1px;
+    left: 0;
     width: 100%;
-    height: 100%;
-    overflow: hidden;
-    background: $color-background;
-    .group {
-        padding-bottom: 30px;
-        .title {
-            height: 30px;
-            line-height: 30px;
-            padding-left: 20px;
-            font-size: $font-size-small;
-            color: $color-text-l;
-            background: $color-highlight-background;
-        }
-        .item {
-            display: flex;
-            align-items: center;
-            padding: 20px 0 0 30px;
-            .avatar {
-                width: 50px;
-                height: 50px;
-                border-radius: 50%;
-            }
-            .name {
-                margin-left: 20px;
-                color: $color-text-l;
-                font-size: $font-size-medium;
-            }
-        }
+    .fixed-title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background: $color-highlight-background;
     }
-    .fixed {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        .fixed-title {
-            height: 30px;
-            line-height: 30px;
-            padding-left: 20px;
-            font-size: $font-size-small;
-            color: $color-text-l;
-            background: $color-highlight-background;
-        }
+  }
+  .shortcut {
+    position: absolute;
+    right: 4px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20px;
+    padding: 20px 0;
+    border-radius: 10px;
+    text-align: center;
+    background: $color-background-d;
+    font-family: Helvetica;
+    .item {
+      padding: 3px;
+      line-height: 1;
+      color: $color-text-l;
+      font-size: $font-size-small;
+      &.current {
+        color: $color-theme;
+      }
     }
-    .shortcut {
-        position: absolute;
-        right: 4px;
-        top: 50%;
-        transform: translateY(-50%);
-        width: 20px;
-        padding: 20px 0;
-        border-radius: 10px;
-        text-align: center;
-        background: $color-background-d;
-        font-family: Helvetica;
-        .item {
-            padding: 3px;
-            line-height: 1;
-            color: $color-text-l;
-            font-size: $font-size-small;
-            &.current {
-                color: $color-theme;
-            }
-        }
-    }
+  }
 }
 </style>
---
+++ src/components/index-list/use-fixed.js
@@ -0,0 +1,60 @@
+import { ref, watch, nextTick, computed } from "vue";
+
+const TITLE_HEIGHT = 30;
+export default function(props) {
+  const scrollY = ref(0);
+  const currentIndex = ref(0);
+  const listHeights = ref([]);
+  const groupRef = ref(null);
+  const distance = ref(0);
+  const fixedTitle = computed(() => {
+    const currentGroup = props.data[currentIndex.value];
+    return currentGroup ? currentGroup.title : "";
+  });
+  const fixedStyle = computed(() => {
+    const diff =
+      distance.value > 0 && distance.value < TITLE_HEIGHT
+        ? distance.value - TITLE_HEIGHT
+        : 0;
+    return {
+      transform: `translate3d(0,${diff}px,0 )`
+    };
+  });
+
+  // 监听props，计算子元素高度到数组listHeights
+  watch(
+    () => props.data,
+    async () => {
+      await nextTick();
+      calculate();
+    }
+  );
+  // 监听滚动事件，去计算currentIndex
+  watch(scrollY, newY => {
+    const listHeightsVal = listHeights.value;
+    for (let i = 0; i < listHeightsVal.length - 1; i++) {
+      const heightTop = listHeightsVal[i];
+      const heightBottom = listHeightsVal[i + 1];
+      if (newY >= heightTop && newY <= heightBottom) {
+        currentIndex.value = i;
+        distance.value = heightBottom - newY;
+      }
+    }
+  });
+  // 计算group子元素高度
+  function calculate() {
+    const list = groupRef.value.children;
+    const listHeightsVal = listHeights.value;
+    let height = 0;
+    listHeightsVal.length = 0;
+    listHeightsVal.push(height);
+    for (let i = 0; i < list.length; i++) {
+      height += list[i].clientHeight;
+      listHeightsVal.push(height);
+    }
+  }
+  function onScroll(position) {
+    scrollY.value = -position.y;
+  }
+  return { onScroll, groupRef, fixedTitle, currentIndex, fixedStyle };
+}
